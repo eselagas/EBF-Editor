f08054ab5905457a0d4cc8f559114fe919a81e81
eselagas <ethanselagea72@outlook.com>
2024-12-23 19:23:38 +1100

    Update ebf_script.js

--- ebf_script.js
+++ ebf_script.js
@@ -8,7 +8,25 @@
     let savedDrawingData = null; // Variable to save drawing data
     let handleScale = 1.0;
     
-    
+function resizeCanvas() {
+    const canvas = document.getElementById('drawingCanvas');
+    if (canvas) {
+        canvas.width = window.innerWidth - 20;
+        canvas.height = window.innerHeight - 108;
+        const context = canvas.getContext('2d');
+        context.clearRect(0, 0, canvas.width, canvas.height);
+    } else {
+        console.error('Canvas element not found');
+    }
+}
+
+// Resize the canvas on window resize
+window.addEventListener('resize', resizeCanvas);
+
+// Initial canvas size
+resizeCanvas();
+
+
     function execCmd(command) {
   if (command === 'indent') {
     document.execCommand('indent', false, null);
